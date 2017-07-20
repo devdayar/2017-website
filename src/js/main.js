7301feda8f7224f71ec53ba3595432219317e342
cristhian <durancristhian@gmail.com>
2017-07-20 14:22:02 -0300

    imrpove header animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 const aload = require('aload')
+const animateOnWindowOnload = document.querySelector('#animate-on-window-onload')
 
-aload()
+window.onload = function () {
+    animateOnWindowOnload.classList.add('fade-in-down-trigger')
+
+    aload()
+}
