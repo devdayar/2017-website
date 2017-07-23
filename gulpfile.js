be224d7f50c38cc156b532df64c3a11c6d2b6618
cristhian <durancristhian@gmail.com>
2017-07-23 13:24:19 -0300

    refactoring of gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,23 @@ function getTask(taskName) {
 
 browserSync.create(config.staticServer.name)
 
-gulp.task('build', ['assets-build', 'compile'])
+gulp.task('browser-sync', getTask('general/browser-sync'))
+
+gulp.task('build', ['html-build'], getTask('manifest/clean'))
 
 gulp.task('assets-build', ['assets-clean', 'assets-copy'])
 gulp.task('assets-clean', getTask('assets/clean'))
 gulp.task('assets-copy', ['assets-clean'], getTask('assets/copy'))
 
-gulp.task('compile', ['html-build'], getTask('manifest/clean'))
-
 gulp.task('css-build', ['css-clean', 'css-lint'], getTask('css/build'))
 gulp.task('css-clean', getTask('css/clean'))
 gulp.task('css-lint', getTask('css/lint'))
 
-gulp.task('html-build', ['html-clean', 'css-build', 'js-build'], getTask('html/build'))
+gulp.task(
+    'html-build',
+    ['assets-build', 'css-build', 'js-build', 'html-clean'],
+    getTask('html/build')
+)
 gulp.task('html-clean', getTask('html/clean'))
 
 gulp.task('js-build', ['js-clean', 'js-lint'], getTask('js/build'))
@@ -32,8 +36,6 @@ gulp.task('js-clean', getTask('js/clean'))
 gulp.task('js-lint', getTask('js/lint'))
 
 gulp.task('manifest-clean', getTask('manifest/clean'))
-
-gulp.task('browser-sync', getTask('general/browser-sync'))
 
 gulp.task('watch', getTask('general/watch'))
 
