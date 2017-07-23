c530cfaa5dc410696adad32a92047ddfc3fd9279
cristhian <durancristhian@gmail.com>
2017-07-23 13:10:00 -0300

    improve gulp by revisioning assets

--- gulp/tasks/assets/copy.js
+++ gulp/tasks/assets/copy.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
+const rev = require('gulp-rev')
 
 module.exports = function(config) {
     return function() {
-        return gulp.src(config.src.assets).pipe(imagemin()).pipe(gulp.dest(config.dest.assets))
+        return gulp
+            .src(config.src.assets)
+            .pipe(imagemin())
+            .pipe(rev())
+            .pipe(gulp.dest(config.dest.assets))
+            .pipe(rev.manifest('manifest.json'))
+            .pipe(gulp.dest(config.dest.assets))
     }
 }
