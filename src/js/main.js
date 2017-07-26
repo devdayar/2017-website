2f93fe698524300742f6227a786de5445bcd5229
cristhian <durancristhian@gmail.com>
2017-07-26 01:55:36 -0300

    improve coc, footer and sponsors

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 const aload = require('aload')
 const animateOnWindowOnload = document.querySelector('#animate-on-window-onload')
+const goToTop = document.querySelector('#go-to-top')
 
 window.onload = function() {
-    animateOnWindowOnload.classList.add('js-fade-in-down-trigger')
+    if (animateOnWindowOnload) {
+        animateOnWindowOnload.classList.add('js-fade-in-down-trigger')
+    }
 
     aload()
+}
+
+if (goToTop) {
+    goToTop.addEventListener('click', function(event) {
+        event.preventDefault()
+
+        window.scrollTo(0, 0)
+    })
 }
